b26a4fb00b4d65f73856bcd60625412468b5709a
Haandsem <145856374+Haandsem@users.noreply.github.com>
2023-09-24 09:03:02 +0100

    Add files via upload

---
+++ W. Mathematical Expression.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char s,q;
+    int a,b,c;
+    cin>>a>>s>>b>>q>>c;
+    if (q=='='){
+        if (s=='+'){
+            if ((c==a+b)==true){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<(a+b)<<endl;
+            }
+        }
+        else if (s=='-'){
+                if ((c==a-b)==true){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<(a-b)<<endl;
+            }
+        }
+        else if (s=='*'){
+            if ((c==a*b)==true){
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<(a*b)<<endl;
+            }
+    }
+
+    }
+    }
